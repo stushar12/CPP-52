2580bec23d4f44401822b282e76c1f5a48d6666d
Tushar Saxena <stushar345@gmail.com>
2021-03-28 15:55:49 +0530

    Update two.cpp

--- two.cpp
+++ two.cpp
@@ -2,33 +2,22 @@
 using namespace std;
 int main()
 {
+
 unordered_map<int,int> ump;
-int p;
-cin>>p;
-while(p--)
-{
-    int n;
-    cin>>n;
-    int arr[n],prev[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-int k=0;
-int flag=0; 
+vector<int> arr{6,-1,-3,4,-2,2,4,6,-12,-7};
+int n=arr.size();
+int k=6; 
 int count=0;
 int sum=0;
 for (int i=0;i<n;i++)
 {
     sum=sum+arr[i];
     if(sum==k)
-    count++;
+        count++;
 
     if(ump.find(sum-k)!=ump.end())
-    count=count+ump[sum-k];
+        count=count+ump[sum-k];
     ump[sum]++;
 }
 cout<<count<<endl;
-ump.clear();
-}
 }
